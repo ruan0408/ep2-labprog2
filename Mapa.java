6ea00cc87d7f06bf8b96d59d8a33a1e0643a638f
Victor Portella <visportella@gmail.com>
2013-10-05 17:14:36 -0300

    Finalmente comecei a fazer o mapa

--- Mapa.java
+++ Mapa.java
@@ -1,9 +1,25 @@
 public class Mapa
 {
+	int altura, largura;
+	Terreno[][] matriz;
 
-  /*Mapa()
-  {
 
-  }*/
+	public Mapa(int alt, int larg)
+	{
+		matriz = new Terreno[alt][larg];
+		altura = alt;
+		largura = larg;
+
+
+		for(int i = 0; i < alt; i++)
+		{
+			for(int j = 0; j < larg; j++)
+			{
+				matriz[i][j] = new Terreno();
+			}
+		}
+	}
+
+
 
 }
