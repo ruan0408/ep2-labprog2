54149693900a21de05026314cbf28b6bd720ce2c
Ruan Costa <ruan.cocito@gmail.com>
2013-11-07 15:32:01 -0200

    lama antiga e classe vizinhança implementada, ainda sem instruções

--- Vizinhanca.java
+++ Vizinhanca.java
@@ -1,4 +1,34 @@
-public class Vizinhanca
+public class Vizinhanca implements Empilhavel
 {
-	//Será implementado futuramente
+	private Terreno [] viz;
+	
+	public Vizinhanca(Mapa mapa, Posicao pos)
+	{
+		viz = new Terreno [6];
+		int x = pos.getX();
+		int y = pos.getY();
+
+		for(int i = 0; i < 6; i++)
+		{
+			try
+			{			
+				switch(i)
+				{
+					case 0 : viz[i] = mapa.getUp(x, y); break;
+					case 1 : viz[i] = mapa.getUpRight(x, y); break;
+					case 2 : viz[i] = mapa.getDownRight(x, y); break;
+					case 3 : viz[i] = mapa.getDown(x, y); break;
+					case 4 : viz[i] = mapa.getDownLeft(x, y); break;
+					case 5 : viz[i] = mapa.getUpLeft(x, y);
+					default:
+				}
+			}
+			catch(ArrayIndexOutOfBoundsException e){viz[i] = null;}
+		}
+	}
+
+	Terreno[] getVizinhanca()
+	{
+		return this.viz;
+	}
 }
